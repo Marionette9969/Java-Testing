b9c047746c5e6794ad5c92689442a01a97fa2166
Yosua Kurniawan <yosua.kurniawan@erajaya.com>
2025-06-05 16:57:08 +0700

    add helloworld jar

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String txt = "Kembalilah ke dunia fana";
+        System.out.println(txt.toUpperCase());   // Outputs
+        System.out.println(txt.toLowerCase());   // Outputs 
+    }
+}
