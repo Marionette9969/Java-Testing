8cbcbeef74f0c2cf0053621dd45436013dfb306c
Yosua Kurniawan <yosua.kurniawan@erajaya.com>
2025-07-21 10:08:33 +0700

    Add more training method and looping

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
+import static java.lang.System.out;
+
 public class HelloWorld {
+
+
     public static void main(String[] args) {
+        
+
         String txt = "Test String Text";
         System.out.println(txt);
         System.out.println(txt.toUpperCase());   // Outputs Huruf Besar
@@ -18,8 +24,33 @@ public class HelloWorld {
         System.out.println("Float and Double calculation");
         System.out.println("");
         float f1 = 1.3f;
-        double d1 = 10.0;
+        double d1 = 10.0; // it can also be float f1 = 1.3f, double d1 = 10.0;
         double result = f1 + d1; // if double change to float, it will result an error
         System.out.println(result);
+
+
+        System.out.println(""); System.out.println("");
+        System.out.println("Java Comparison Variable");
+        int a1 = 10;
+        a1 += 5;
+        System.out.println(a1);
+
+
+        System.out.println(""); System.out.println("");
+        System.out.println("Java Special Character");
+        String Singlequote = "For a single quote char will need to have = \\' ";
+        String Doublequote = "For a double quote char will need to have = \\\"\"\\";
+        System.out.println(Singlequote);
+        out.println(Doublequote);
+
+        out.println("");
+        out.println("");
+        out.println("Java While Loop Testing ");
+        
+        int i = 10;
+        for (i = 0 ; i < 10; i++)
+        {
+            out.print(i);
+        }
     }
 }
